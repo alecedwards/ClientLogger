a1704a4e1a8ee9d37064822e4bc284e74bff55d1
Alec Edwards <ealec95@gmail.com>
2019-09-22 11:27:49 -0400

    Some stuff

---
+++ ClientLogger/ClientLogger/Controllers/CredentialsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace ClientLogger.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CredentialsController : Controller
+    {
+        
+        [HttpGet]
+        public Task<IActionResult> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
